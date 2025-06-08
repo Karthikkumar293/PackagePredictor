ee0c44b7773739502736f912df8bd972d2dddd20
Karthikkumar293 <kumarkarthik5665@gmail.com>
2025-06-08 17:34:12 +0530

    Add files via upload

---
+++ karthik1.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+
+# Set seed for reproducibility
+np.random.seed(42)
+
+# for genrate a random cgpa nuber
+cgpa = np.round(np.random.uniform(6.0, 10.0, 1000), 2)
+
+# for genrate package
+noise = np.random.normal(0, 1.0, 1000)
+package = np.round(cgpa * 1.5 + noise, 2)
+package = np.clip(package, 2.0, 30.0)  # Limiting values to realistic ranges
+
+# Create DataFrame
+df = pd.DataFrame({
+    "CGPA": cgpa,
+    "Package (LPA)": package
+})
+
+df.head(1000)
+
+df.isnull()
+
+x =df[["CGPA"]]
+y=df["Package (LPA)"]
+
+import seaborn as sns
+import matplotlib as plt
+from sklearn.model_selection import train_test_split
+
+sns.scatterplot(x="CGPA",y="Package (LPA)",data=df)
+
+ x_train,x_test,y_train,y_test =train_test_split(x,y,test_size=0.25,random_state=30)
+
+  karthik.fit(x_train,y_train)
+
+karthik.predict([[7.9]])
