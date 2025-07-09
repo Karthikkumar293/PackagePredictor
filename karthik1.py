96ad4307e2e17014868f3b45366de82e1a1db771
Karthikkumar293 <kumarkarthik5665@gmail.com>
2025-07-09 12:14:45 +0530

    Update karthik1.py

--- karthik1.py
+++ karthik1.py
@@ -31,7 +31,7 @@ from sklearn.model_selection import train_test_split
 sns.scatterplot(x="CGPA",y="Package (LPA)",data=df)
 
 x_train,x_test,y_train,y_test =train_test_split(x,y,test_size=0.25,random_state=30)
-from  sklearn import LinearRegression
+from  sklearn.linear_model import LinearRegression
 karthik = LinearRegression() 
 karthik.fit(x_train,y_train)
 
